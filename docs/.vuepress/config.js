8a1e0266ee4f8ab00d094f9dc3e8092388b3a155
phoenixpeca <aspaciop@outlook.com>
2020-02-27 18:32:44 +0800

    updated config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,19 +5,26 @@ module.exports = {
       title: null,
       description: 'RAKwireless Document Center'
     },
-    '/zh-cn/': {
-      lang: 'zh-CN',
-      title: null,
-      description: 'RAKwireless Document Center'
-    }
+    // '/zh-cn/': {
+    //   lang: 'zh-CN',
+    //   title: null,
+    //   description: 'RAKwireless Document Center'
+    // }
   },
   themeConfig: {
     logo: '/rakwireless/rak-blue-dark.svg',
     sidebar: 'auto',
+    repo: 'RAKwirelessDev/rakwireless-docs',
+    docsDir: 'docs',
+    docsBranch: 'master',
+    editLinks: true,
+    smoothScroll: true,
     locales: {
       '/en-us/': {
         selectText: 'Languages',
         label: 'English',
+        editLinkText: 'Edit this page in GitHub',
+        lastUpdated: 'Last Updated',
         nav: [
           { text: 'Home', link: '/en-us/' },
           { text: 'Quick Start', link: '/zh-cn/quick-start/' },
@@ -28,19 +35,21 @@ module.exports = {
           { text: 'Forum', link: 'https://forum.rakwireless.com/' }
         ]
       },
-      '/zh-cn/': {
-        selectText: '选择语言',
-        label: '简体中文',
-        nav: [
-          { text: '家', link: '/zh-cn/' },
-          { text: '快速开始', link: '/zh-cn/quick-start/' },
-          { text: '数据表', link: '/zh-cn/datasheet/' },
-          { text: 'RUI', link: '/zh-cn/rui/' },
-          { text: '下载中心', link: 'https://downloads.rakwireless.com/' },
-          { text: '商店', link: 'https://store.rakwireless.com/' },
-          { text: '论坛', link: 'https://forum.rakwireless.com/' }
-        ]
-      }
+      // '/zh-cn/': {
+      //   selectText: '选择语言',
+      //   label: '简体中文',
+      //   editLinkText: '在 GitHub 上编辑此页',
+      //   lastUpdated: '上次更新',
+      //   nav: [
+      //     { text: '家', link: '/zh-cn/' },
+      //     { text: '快速开始', link: '/zh-cn/quick-start/' },
+      //     { text: '数据表', link: '/zh-cn/datasheet/' },
+      //     { text: 'RUI', link: '/zh-cn/rui/' },
+      //     { text: '下载中心', link: 'https://downloads.rakwireless.com/' },
+      //     { text: '商店', link: 'https://store.rakwireless.com/' },
+      //     { text: '论坛', link: 'https://forum.rakwireless.com/' }
+      //   ]
+      // }
     }
   }
 }
