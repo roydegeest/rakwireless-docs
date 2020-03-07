98d1df4427f745ea6ce0a6993cda9489ccb35f6d
phoenixpeca <aspaciop@outlook.com>
2020-03-07 09:10:24 +0800

    updated

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,12 +5,15 @@ module.exports = {
       title: 'RAKwireless Document Center',
       description: 'RAKwireless Knowledge Hub'
     },
-    // '/zh-cn/': {
-    //   lang: 'zh-CN',
-    //   title: null,
-    //   description: 'RAKwireless Document Center'
-    // }
+    '/zh-cn/': {
+      lang: 'zh-CN',
+      title: 'RAKwireless Document Center',
+      description: 'RAKwireless Knowledge Hub'
+    }
   },
+  head: [
+    ['link', { rel: 'icon', href: `/favicon.ico` }]
+  ],
   themeConfig: {
     logo: '/assets/rakwireless/rak-blue-dark.svg',
     sidebar: 'auto',
@@ -35,21 +38,21 @@ module.exports = {
           { text: 'Forum', link: 'https://forum.rakwireless.com/' }
         ]
       },
-      // '/zh-cn/': {
-      //   selectText: '选择语言',
-      //   label: '简体中文',
-      //   editLinkText: '在 GitHub 上编辑此页',
-      //   lastUpdated: '上次更新',
-      //   nav: [
-      //     { text: '家', link: '/zh-cn/' },
-      //     { text: '快速开始', link: '/zh-cn/quick-start/' },
-      //     { text: '数据表', link: '/zh-cn/data-sheet/' },
-      //     { text: 'RUI', link: '/zh-cn/rui/' },
-      //     { text: '下载中心', link: 'https://downloads.rakwireless.com/' },
-      //     { text: '商店', link: 'https://store.rakwireless.com/' },
-      //     { text: '论坛', link: 'https://forum.rakwireless.com/' }
-      //   ]
-      // }
+      '/zh-cn/': {
+        selectText: '选择语言',
+        label: '简体中文',
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdated: '上次更新',
+        nav: [
+          { text: '家', link: '/zh-cn/' },
+          { text: '快速开始', link: '/zh-cn/quick-start/' },
+          { text: '数据表', link: '/zh-cn/data-sheet/' },
+          { text: 'RUI', link: '/zh-cn/rui/' },
+          { text: '下载中心', link: 'https://downloads.rakwireless.com/' },
+          { text: '商店', link: 'https://store.rakwireless.com/' },
+          { text: '论坛', link: 'https://forum.rakwireless.com/' }
+        ]
+      }
     }
   },
   plugins: {
