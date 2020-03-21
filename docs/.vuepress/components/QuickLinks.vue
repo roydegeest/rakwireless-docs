878aaa25f180f67711f75877d4a90946b97f2452
Carl Richard Dumdum <d.richardcarl@gmail.com>
2020-03-21 18:16:32 +0800

    initial commit for quicklinks component and added nodemon for realtime code compile

---
+++ docs/.vuepress/components/QuickLinks.vue
@@ -0,0 +1,34 @@
+<template>
+  asdas
+  <div class="row">
+    <div clas="col">
+      <div><strong>Learn More</strong></div>
+      <ul>
+        <li
+          v-for="(link, id) in links.learnMore"
+          :key="id"
+        >
+          <a :href="link.href">{{ link.name }}</a>
+        </li>
+      </ul>
+    </div>
+    <div class="col">
+      <div><strong>Resources</strong></div>
+      <ul>
+        <li
+          v-for="(link, id) in links.resources"
+          :key="id"
+        >
+          <a :href="link.href">{{ link.name }}</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuickLinks',
+  props: ['links']
+}
+</script>
