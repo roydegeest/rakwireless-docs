c0c498fc17421b82602fa519d21119506253cad7
MarkNambatac <nambatac80@gmail.com>
2020-04-21 08:28:40 +0800

    implement new navigation bar

--- docs/.vuepress/includes/nav/en-us.js
+++ docs/.vuepress/includes/nav/en-us.js
@@ -2,9 +2,11 @@ module.exports = [
     { text: 'Home', link: '/en-us/' },
     { text: 'Quick Start', link: '/en-us/quick-start/' },
     //{ text: 'Datasheet', link: '/en-us/datasheet/' },
+    // { text: 'RUI', link: '/en-us/rui/' },
+    // { text: 'Downloads', link: 'https://downloads.rakwireless.com/' },
+    // { text: 'Store', link: 'https://store.rakwireless.com/' },
+    {text: 'User Manual', link: '/en-us/user-manual/'},
+    {text: 'Deployment Guide', link : '/en-us/deployment-guide/'},
     { text: 'Datasheet', link: '/en-us/datasheet/rak831/#rak831-lpwan-gateway-concentrator' },
-    { text: 'RUI', link: '/en-us/rui/' },
-    { text: 'Downloads', link: 'https://downloads.rakwireless.com/' },
-    { text: 'Store', link: 'https://store.rakwireless.com/' },
     { text: 'Forum', link: 'https://forum.rakwireless.com/' }
 ]
