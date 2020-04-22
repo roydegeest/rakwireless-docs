7fa8a6df1b3d3707ae74374d232c23bc68154d40
Carl Richard Dumdum <d.richardcarl@gmail.com>
2020-04-22 09:35:29 +0800

    fixed conflicts

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,20 +16,7 @@ module.exports = {
       rel: 'icon',
       href: `/favicon.ico`
     }],
-<<<<<<< HEAD
     ['script', { src: '/js/livechat.js' }]
-=======
-    ['script', {}, `
- 
-    function initFreshChat() {
-      window.fcWidget.init({
-        token: "15eb72a9-7ba6-43c6-9923-4a6769c7b9bc",
-        host: "https://wchat.freshchat.com"
-      });
-    }
-    function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
-    `]
->>>>>>> f9079a55e9ec2b7bdd07878a8e12e468bb666fae
   ],
   themeConfig: {
     logo: '/assets/rakwireless/rak-blue-dark.svg',
