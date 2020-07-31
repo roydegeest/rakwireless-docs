87fbe4c807f6a68a46ab4d35832de3b29f4555bb
d.richardcarl <d.richardcarl@gmail.com>
2020-08-01 01:15:51 +0800

    Update: intro page

--- docs/.vuepress/theme/components/RkFooter.vue
+++ docs/.vuepress/theme/components/RkFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-white text-grey-7 q-pa-md" style="padding: 2.5rem 0">
     <div class="full-height flex flex-center q-py-md">
-      <img :src="`/assets/rakwireless/rak-blue-dark.svg`" style="width: 12.5rem" />
+      <img :src="`/assets/rakwireless/RAK-only-gray.svg`" style="width: 12.5rem" />
     </div>
     <div
       class="q-mx-md text-center"
